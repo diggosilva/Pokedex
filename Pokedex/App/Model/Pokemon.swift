8f3344dd577bdd50bd41fd97815d9fd0d9edaada
Diggo <rdiggosilva@gmail.com>
2024-05-26 15:01:01 -0300

    Pagination finished

--- Pokedex/App/Model/Pokemon.swift
+++ Pokedex/App/Model/Pokemon.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Pokemon {
+struct Pokemon: Equatable {
     let name: String
     let url: String
     
--- Pokedex/App/Network/Service.swift
+++ Pokedex/App/Network/Service.swift
@@ -9,13 +9,13 @@ import Foundation
 
 protocol ServiceProtocol {
     var dataTask: URLSessionDataTask? { get set }
-    func getPokemons(url: String, onSuccess: @escaping([Pokemon]) -> Void, onError: @escaping(Error) -> Void)
+    func getPokemons(url: String, onSuccess: @escaping(String?, [Pokemon]) -> Void, onError: @escaping(Error) -> Void)
 }
 
 final class Service: ServiceProtocol {
     var dataTask: URLSessionDataTask?
     
-    func getPokemons(url: String, onSuccess: @escaping([Pokemon]) -> Void, onError: @escaping(Error) -> Void) {
+    func getPokemons(url: String, onSuccess: @escaping(String?, [Pokemon]) -> Void, onError: @escaping(Error) -> Void) {
         guard let url = URL(string: url) else { return }
         
         dataTask = URLSession.shared.dataTask(with: url, completionHandler: { data, response, error in
@@ -27,12 +27,13 @@ final class Service: ServiceProtocol {
                 do {
                     let pokemonResponse = try JSONDecoder().decode(PokemonResponse.self, from: data ?? Data())
                     var pokemons: [Pokemon] = []
+                    let nextUrl = pokemonResponse.next
                     
                     for pokemon in pokemonResponse.results {
                         pokemons.append(Pokemon(name: pokemon.name, url: pokemon.url))
                     }
                     
-                    onSuccess(pokemons)
+                    onSuccess(nextUrl, pokemons)
                     print("DEBUG: SUCESSO ao decodificar POKEMONS \(pokemons)")
                 } catch {
                     onError(error)
