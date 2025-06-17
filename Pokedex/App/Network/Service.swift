32706cf1e3b39ee4e7a7726d853d8eabb194ed8e
Diggo Silva <rdiggosilva@gmail.com>
2025-06-17 10:21:57 -0300

    Implemented Async Throws on getDetails method

--- Pokedex/App/Network/Service.swift
+++ Pokedex/App/Network/Service.swift
@@ -9,7 +9,7 @@ import Foundation
 
 protocol ServiceProtocol {
     func getPokemons(url: String) async throws -> (String?, [FeedModel])
-    func getDetails(id: Int, onSuccess: @escaping(DetailModel) -> Void, onError: @escaping(Error) -> Void)
+    func getDetails(id: Int) async throws -> DetailModel
 }
 
 final class Service: ServiceProtocol {
@@ -39,34 +39,36 @@ final class Service: ServiceProtocol {
         }
     }
     
-    func getDetails(id: Int, onSuccess: @escaping(DetailModel) -> Void, onError: @escaping(Error) -> Void) {
-        guard let url = URL(string: "https://pokeapi.co/api/v2/pokemon/\(id)") else { return }
+    func getDetails(id: Int) async throws -> DetailModel {
+        guard let url = URL(string: "https://pokeapi.co/api/v2/pokemon/\(id)") else {
+            throw NSError(domain: "Invalid URL", code: 0, userInfo: nil)
+        }
         
-        URLSession.shared.dataTask(with: url, completionHandler: { data, response, error in
-            DispatchQueue.main.async {
-                if let data = data {
-                    do {
-                        let detailsResponse = try JSONDecoder().decode(DetailResponse.self, from: data)
-                        var pokemonStats: [PokemonStats] = []
-                        
-                        for stat in detailsResponse.stats {
-                            pokemonStats.append(PokemonStats(base: Int(Double(stat.baseStat)), name: stat.stat.name))
-                        }
-                        let detailModel = DetailModel(
-                            name: detailsResponse.name,
-                            weight: Double(detailsResponse.weight),
-                            types: detailsResponse.types[0].type.name,
-                            stats: pokemonStats,
-                            image: detailsResponse.sprites.other?.officialArtwork.frontDefault ?? "",
-                            height: Double(detailsResponse.height)
-                        )
-                        onSuccess(detailModel)
-                    } catch {
-                        onError(error)
-                        print("DEBUG: Erro aos decodificar DETALHES.. \(error.localizedDescription)")
-                    }
-                }
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        if let response = response as? HTTPURLResponse {
+            print("DEBUG: Statuscode.. \(response.statusCode)")
+        }
+        
+        do {
+            let detailsResponse = try JSONDecoder().decode(DetailResponse.self, from: data)
+            var pokemonStats: [PokemonStats] = []
+            
+            for stat in detailsResponse.stats {
+                pokemonStats.append(PokemonStats(base: Int(Double(stat.baseStat)), name: stat.stat.name))
             }
-        }).resume()
+            let detailModel = DetailModel(
+                name: detailsResponse.name,
+                weight: Double(detailsResponse.weight),
+                types: detailsResponse.types[0].type.name,
+                stats: pokemonStats,
+                image: detailsResponse.sprites.other?.officialArtwork.frontDefault ?? "",
+                height: Double(detailsResponse.height)
+            )
+            return detailModel
+        } catch {
+            print("DEBUG: Erro aos decodificar DETALHES.. \(error.localizedDescription)")
+            throw error
+        }
     }
 }
